afe35576dc0ab2690e298b077396ccc2517c830a
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2015-04-25 18:02:27 +0300

    Polynomial

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,15 @@
 package calculator
 
+import scala.collection.immutable.HashSet
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    new Signal[Double](b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    new Signal[Set[Double]](new HashSet[Double]() + ((-b() + Math.sqrt(delta()))/(2 * a())) + ((-b() - Math.sqrt(delta()))/(2 * a())))
   }
 }
